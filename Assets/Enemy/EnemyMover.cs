578bc6063b94f3efa5b4eec27eb2e9bab1caa534
Robert <robertjwisniowski@gmail.com>
2021-08-18 18:21:14 +0200

    The enemy movement system has been upgraded.
    The enemy moves smoothly and turns frontally toward its pathway. Variants of tile prefab have been added.

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f,5f)]float speed = 1f;
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
 
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime*speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 
