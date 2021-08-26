84480f72c2072a2ee1cb30d8c24c06e5e61c8d85
Robert <robertjwisniowski@gmail.com>
2021-08-26 22:03:13 +0200

    The targeting system has been upgraded.
    The ballistics  target the closest ram.

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -5,24 +5,63 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
-    Transform weapon;
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem ProjectileParticles;
+    [SerializeField] Transform weapon;
     Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-        weapon = transform.Find("BallistaTopMesh");
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(targetDistance<maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
-        weapon.LookAt(target);
+        if(target==null)
+        {
+            Attack(false);
+        }
+        else
+        {
+            float targetDistance = Vector3.Distance(transform.position, target.position);
+            weapon.LookAt(target);
+
+            if(targetDistance<range)
+            {
+                Attack(true);
+            }
+            else
+            {
+                Attack(false);
+            }
+        }
+
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = ProjectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
