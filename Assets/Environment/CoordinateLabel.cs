df32243335e90f69496af917bf1850a2e3722447
Robert <robertjwisniowski@gmail.com>
2021-09-07 19:49:52 +0200

    The coordinates can take on different colours.

--- Assets/Environment/CoordinateLabel.cs
+++ Assets/Environment/CoordinateLabel.cs
@@ -9,26 +9,29 @@ public class CoordinateLabel : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if(!Application.isPlaying)
+        if (!Application.isPlaying)
         {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
         }
 
         SetLabelColor();
@@ -37,30 +40,46 @@ public class CoordinateLabel : MonoBehaviour
 
     void ToggleLabels()
     {
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             label.enabled = !label.IsActive();
         }
     }
 
+
     void SetLabelColor()
     {
-        if(waypoint.IsPlaceable)
+            if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+
+        if (!node.isWalkable)
         {
-            label.color = defaultColor;
+            label.color = blockedColor;
+        }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
         }
         else
         {
-            label.color = blockedColor;
+
+            label.color = defaultColor;
         }
     }
 
     void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x/ UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z/ UnityEditor.EditorSnapSettings.move.z);
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
 
-        label.text = coordinates.x+","+coordinates.y;
+        label.text = coordinates.x + "," + coordinates.y;
     }
 
     void UpdateObjectName()
--- Assets/Pathfinding/Node.cs
+++ Assets/Pathfinding/Node.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [System.Serializable]
 public class Node
 {
-    public Vector2 coordinates;
+    public Vector2Int coordinates;
     public bool isWalkable;
     public bool isExplored;
     public bool isPath;
     public Node connectedTo;
 
-     public Node(Vector2 coordinates, bool isWalkable)
+     public Node(Vector2Int coordinates, bool isWalkable)
     {
         this.coordinates = coordinates;
         this.isWalkable = isWalkable;
